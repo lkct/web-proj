8d8ed6202eed047b7a8ab48dc824537082386ad4
PblackT <1037430748@qq.com>
2019-01-10 19:16:09 +0800

    update

--- Main_action.js
+++ Main_action.js
@@ -3,13 +3,13 @@
 */
 
 // 下载文件
-function Download(){
+function Download(path, filename){
     var auth = new URLSearchParams();         
     var params = new URLSearchParams();
     var formData = new FormData();
     params.append("func", "download");
-    params.append("path", "/abc/");
-    params.append("filename", "a.py");
+    params.append("path", path);
+    params.append("filename", filename);
     formData.append("auth", auth);
     formData.append("params", params);
     $.ajax({
@@ -38,13 +38,11 @@ function Display_the_files(files){
         var para = document.createElement("li");
         if (is_dir != 0) {
             para.innerHTML = '<section class="cd-section" style="margin-top: 50px;">'
-					+'<button class="cd-bouncy-nav-trigger" type="button" id='+filename+' onclick="pas()">'+filename + '</button></section>'
+					+'<button class="cd-bouncy-nav-trigger" type="button" onclick="pasd('+filename+')">'+filename + '</button></section>'
 					+'<div class="cd-bouncy-nav-modal">'
 					+'<nav><ul class="cd-bouncy-nav">'
-					+'<li id="enter">Enter</li>'
-					+'<li id="copy">Copy</li>'
-					+'<li id="cut">Cut</li>'
-					+'<li id="delete">Delete</li>'
+					+'<li class="enter">Enter</li>'
+					+'<li class="copy">Copy</li>'
 					+'</ul></nav>'
                     +'<a class="cd-close">Close modal</a></div>';
             para.style.color='blue'
@@ -52,14 +50,12 @@ function Display_the_files(files){
         }
         else {
             para.innerHTML = '<section class="cd-section" style="margin-top: 50px;">'
-					+'<button class="cd-bouncy-nav-trigger" type="button" id='+filename+' onclick="pas()">'+filename + '</button></section>'
+					+'<button class="cd-bouncy-nav-trigger" type="button" onclick="pasf('+filename+')">'+filename + '</button></section>'
 					+'<div class="cd-bouncy-nav-modal">'
 					+'<nav><ul class="cd-bouncy-nav">'
-					+'<li id="enter">Enter</li>'
-					+'<li id="down">Download</li>'
-					+'<li id="copy">Copy</li>'
-					+'<li id="cut">Cut</li>'
-					+'<li id="delete">Delete</li>'
+					+'<li class="down">Download</li>'
+					+'<li class="copy">Copy</li>'
+					+'<li class="delete">Delete</li>'
 					+'</ul></nav>'
 					+'<a class="cd-close">Close modal</a></div>';
             element.appendChild(para);
@@ -67,7 +63,7 @@ function Display_the_files(files){
     }
 }
 
-function pas(){
+function pasd(dname){
 	jQuery(document).ready(function($){
 		var is_bouncy_nav_animating = false;
 		//open bouncy navigation
@@ -77,7 +73,70 @@ function pas(){
 		//close bouncy navigation
 		$('.cd-bouncy-nav-modal .cd-close').on('click', function(){
 			triggerBouncyNav(false);
+        });
+        $('.cd-bouncy-nav-modal .cd-bouncy-nav .copy').on('click', function(){
+            localStorage.cp_src_path = localStorage.path;
+            localStorage.cp_src_dir = dname;
+            triggerBouncyNav(false);
+            // waiting for the"paste" to call the Copy function
+        });
+        $('.cd-bouncy-nav-modal .cd-bouncy-nav .enter').on('click', function(){
+            localStorage.path = localStorage.path + '/' + dname;
+            window.location.href = window.location.href
+        });
+		$('.cd-bouncy-nav-modal').on('click', function(event){
+			if($(event.target).is('.cd-bouncy-nav-modal')) {
+				triggerBouncyNav(false);
+			}
 		});
+
+		function triggerBouncyNav($bool) {
+			//check if no nav animation is ongoing
+			if( !is_bouncy_nav_animating) {
+				is_bouncy_nav_animating = true;
+				
+				//toggle list items animation
+				$('.cd-bouncy-nav-modal').toggleClass('fade-in', $bool).toggleClass('fade-out', !$bool).find('li:last-child').one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(){
+					$('.cd-bouncy-nav-modal').toggleClass('is-visible', $bool);
+					if(!$bool) $('.cd-bouncy-nav-modal').removeClass('fade-out');
+					is_bouncy_nav_animating = false;
+				});
+				
+				//check if CSS animations are supported... 
+				if($('.cd-bouncy-nav-trigger').parents('.no-csstransitions').length > 0 ) {
+					$('.cd-bouncy-nav-modal').toggleClass('is-visible', $bool);
+					is_bouncy_nav_animating = false;
+				}
+			}
+		}
+	});
+}
+
+function pasf(fname){
+	jQuery(document).ready(function($){
+		var is_bouncy_nav_animating = false;
+		//open bouncy navigation
+		$('.cd-bouncy-nav-trigger').on('click', function(){
+			triggerBouncyNav(true);
+		});
+		//close bouncy navigation
+		$('.cd-bouncy-nav-modal .cd-close').on('click', function(){
+			triggerBouncyNav(false);
+        });
+        $('.cd-bouncy-nav-modal .cd-bouncy-nav .copy').on('click', function(){
+			localStorage.cp_src_path = localStorage.path;
+            localStorage.cp_src_file = fname;
+            triggerBouncyNav(false);
+            // waiting for the"paste" to call the Copy function
+        });
+        $('.cd-bouncy-nav-modal .cd-bouncy-nav .delete').on('click', function(){
+            Delete_file(localStorage.path, fname);
+			triggerBouncyNav(false);
+        });
+        $('.cd-bouncy-nav-modal .cd-bouncy-nav .down').on('click', function(){
+            Download(localStorage.path, fname);
+			triggerBouncyNav(false);
+        });
 		$('.cd-bouncy-nav-modal').on('click', function(event){
 			if($(event.target).is('.cd-bouncy-nav-modal')) {
 				triggerBouncyNav(false);
@@ -127,7 +186,7 @@ function Delete_file(Path, Filename){
             if(errno==1)
                 alert("Failed");
             else
-                window.location.href = Path; // 可以采用refresh？
+                window.location.href = window.location.href;
         },
         error: function (xhr) {
             alert(xhr.status + " " + xhr.statusText + "\n"
